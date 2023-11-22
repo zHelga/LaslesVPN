375494346d26cad879a9e85581b71420fc2cd93d
zHelga <zubacolga1998/@gmail.com>
2023-11-22 13:56:32 +0200

    deleted one comment in js

--- js/script.js
+++ js/script.js
@@ -51,42 +51,6 @@ for ( let i = 0; i < footerItems.length; i++) {
 }
 
 // ---------------------------------------------------------------
-/* const animItems = document.querySelectorAll('._anim-items');
-
-if (animItems.length > 0) {
-	window.addEventListener('scroll', animOnScroll);
-	function animOnScroll() {
-		for (let i = 0; i < animItems.length; i++) {
-			const animItem = animItems[i];
-			const animItemHeight = animItem.offsetHeight;
-			const animItemOffset = offset(animItem).top;
-			const animStart = 4;
-
-			let animItemPoint = window.innerHeight - animItemHeight / animStart;
-			if (animItemHeight > window.innerHeight) {
-				animItemPoint = window.innerHeight - window.innerHeight / animStart;
-			}
-
-			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-				animItem.classList.add('_active');
-				console.log(animItem)
-			} else {
-				
-				if(!animItem.classList.contains('_anim-no-hide')) {
-					animItem.classList.remove('_active')
-				}
-			}
-		}
-	}
-	function offset(el) {
-		const rect = el.getBoundingClientRect(),
-			scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-			scrollTop = window.scrollY || document.documentElement.scrollTop;
-		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
-	}
-	animOnScroll();
-} */
-
 document.addEventListener('DOMContentLoaded', () => {
 	const scrollItems = document.querySelectorAll('.scroll-item');
 
