c81cd5f08c89a15e0c097db2fa8e133a2aa2404f
zHelga <zubacolga1998/@gmail.com>
2023-11-22 13:55:01 +0200

    added animation during scroll

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ let footerItems = document.querySelectorAll('.footer-item-move');
 
 for ( let i = 0; i < footerItems.length; i++) {
   footerItems[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("footer-item-move_active");
     let panel = this.querySelector('.footer__list');
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
@@ -49,3 +49,66 @@ for ( let i = 0; i < footerItems.length; i++) {
     } 
   });
 }
+
+// ---------------------------------------------------------------
+/* const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let i = 0; i < animItems.length; i++) {
+			const animItem = animItems[i];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+				console.log(animItem)
+			} else {
+				
+				if(!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active')
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+			scrollTop = window.scrollY || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+	animOnScroll();
+} */
+
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollItems = document.querySelectorAll('.scroll-item');
+
+	const scrollAnimation = () => {
+		let windowCenter = (window.innerHeight / 2) + window.scrollY;
+
+		scrollItems.forEach(el => {
+			let scrollOffset = el.offsetTop + (el.offsetHeight / 10);
+			console.log(scrollOffset)
+			if(windowCenter >= scrollOffset) {
+				el.classList.add('animation-class')
+			} else {
+				if(!el.classList.contains('_hide')) {
+					el.classList.remove('animation-class')
+				}
+			}
+		})
+		
+	}
+
+	// scrollAnimation();
+	window.addEventListener('scroll', () => {
+		scrollAnimation()
+	})
+})
